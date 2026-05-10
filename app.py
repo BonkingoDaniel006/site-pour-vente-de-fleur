bb0da39b1546a2669afe70b52632ec783e3b82c4
Daniel Bokingo Botay <bokingodaniel4@gmail.com>
2026-05-10 15:32:55 +0200

    feat:database-connection

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Daniel12349",
+    database="site_fleurs"
+)
+cursor = conn.cursor(dictionary=True)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
